4f96a2dcdaf349e1f9792193f300a87a9dd0fdef
fauzynurn <fnnoviansyah@gmail.com>
2018-03-29 21:12:16 +0700

    Unfinished. intent not set up completely.

--- app/src/main/java/com/example/odoo/minimalproject/MainActivity.java
+++ app/src/main/java/com/example/odoo/minimalproject/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.odoo.minimalproject;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,18 +10,26 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.jaeger.library.StatusBarUtil;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText nim;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        new statusBarConfig().StatusBarLightMode(this);
-        Button moveBtn = findViewById(R.id.move_button);
+        ImageView moveBtn = findViewById(R.id.move_button);
+        nim = findViewById(R.id.inputNim);
+        getWindow().setStatusBarColor(Color.TRANSPARENT);
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
 
         moveBtn.setOnClickListener(new View.OnClickListener() {
 
@@ -28,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View arg0) {
                 // TODO Auto-generated method stub
                 Intent inte = new Intent(MainActivity.this, layoutActivity.class);
+                inte.putExtra("nim",nim.getText().toString());
                 startActivity(inte);
             }
         });
